df5ed4ad3abe07266c4d2898ba877d2ba956cf6d
Kamaljit Kaur <76kaur11@gmail.com>
2025-06-08 15:30:59 +0530

    Prepare app for Heroku deployment

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/download_resume')
+def download_resume():
+    return send_file("Kamaljit_Kaur.pdf", as_attachment=True)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
